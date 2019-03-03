230e20239dca4851b1526ae9396c5c9be1f4e6b6
erikkroes <github@k4rp.nl>
2019-03-03 22:30:37 +0100

    Basic structure and start of functionality

--- src/color-tool.js
+++ src/color-tool.js
@@ -1,5 +1,5 @@
+/* eslint-disable class-methods-use-this */
 import { LitElement, html, css } from 'lit-element';
-import { openWc } from './open-wc-logo';
 
 class ColorTool extends LitElement {
   static get properties() {
@@ -10,47 +10,124 @@ class ColorTool extends LitElement {
 
   constructor() {
     super();
-    this.title = 'open-wc';
+    this.title = 'color-tool';
+    this.selectedSwatch = 0;
+    this.swatches = [
+      { x: 0.9505,
+      y: 1.0000,
+      z: 1.0888,
+      hex: '#ffffff',
+      contrast: '1.0:1'}
+    ];
+    this.color = {
+      xyz : { x: 0.9505, y: 1.0000, z: 1.0888 },
+      rgb : { r: 255, g: 255, b: 255},
+      hex: '#ffffff',
+      value: "#ffffff"
+    }
   }
 
   static get styles() {
     return [
       css`
         :host {
-          text-align: center;
-          min-height: 100vh;
+          display: block;          
+          max-width: 960px;
+
+          font-size: 16px;
+        }
+
+        .card {
+          border-radius: 2px;
+          box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24); // https://codepen.io/sdthornton/pen/wBZdXq
+        }
+
+        .swatches__fieldset {
+          border: 0;
+          margin: 0;
+          padding: 0;
+        }
+
+        .swatches__group {
+          display: flex;
+          justify-content: flex-start;    
+          margin-bottom: 16px;   
+        }
+
+        .swatch {
           display: flex;
           flex-direction: column;
-          align-items: center;
+          justify-content: flex-end;
+          min-height: 120px;
+          min-width: 140px;
+          margin-right: 8px;
+        }
+
+        .swatch--selected {
+          box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+        }
+
+        .swatch--button {
+          
+        }
+
+        .swatch__add-button {
+          all: inherit;
+          display: flex;
           justify-content: center;
-          font-size: calc(10px + 2vmin);
-          color: #1a2b42;
+          min-height: 120px;
+          min-width: 140px;
+          border: 1px solid transparent;
+          width; 100%;
         }
 
-        header {
-          margin: auto;
+        .swatch__add-button:focus {
+          border: 1px solid #005F62;
+          box-shadow: 0 0 16px #005F62;
         }
 
-        svg {
-          animation: app-logo-spin infinite 20s linear;
+        .swatch__radio {
+          opacity: 0;
+          width: 1px;
         }
 
-        a {
-          color: #217ff9;
+        .swatch__label {
+          display: flex;
+          justify-content: space-between;
         }
 
-        .app-footer {
-          color: #a8a8a8;
-          font-size: calc(10px + 0.5vmin);
+
+        .color {
+          display: grid;
+          grid-template-columns: 1fr 1fr 1fr;   
+          grid-gap: 16px;       
         }
 
-        @keyframes app-logo-spin {
-          from {
-            transform: rotate(0deg);
-          }
-          to {
-            transform: rotate(360deg);
-          }
+        .color__input-holder {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          grid-column: span 3;
+          min-height: 240px;
+        }
+
+        .color__input {
+          font-size: 24px;
+          padding: 8px;
+          border-radius: 8px;
+          border-style: solid;
+        }
+
+        .color__block {
+          max-width: 300px;
+          padding: 24px;
+          background-color: #ffffff;
+        }
+
+        .color__range {
+          display: flex;
+          justify-content: space-between;
+
         }
       `,
     ];
@@ -58,25 +135,114 @@ class ColorTool extends LitElement {
 
   render() {
     return html`
-      <header class="app-header">
-        ${openWc}
+      <header>
+        
         <h1>${this.title}</h1>
-        <p>Edit <code>src/my-app.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/developing/#examples"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
+
       </header>
-      <p class="app-footer">
-        🚽 Made with love by
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/open-wc">open-wc</a>.
-      </p>
+      <section class="swatches">       
+          <h2 class="swatches__title">Swatches</h2>
+          <fieldset class="swatches__fieldset">
+            <div class="swatches__group">
+              ${this.swatches.map((swatch, i) => this._swatchTemplate(swatch, i))}
+              
+              <div class="card swatch--button">
+                <button @click=${this._addSwatch} class="swatch__add-button">+</button> 
+              </div>                           
+            </div>
+          </fieldset>
+      </section>
+
+      <main class="color">
+        <div class="card color__input-holder" style="background-color: ${this.color.hex}">
+          <input type="text" class="color__input" value="${this.color.value}">
+        </div>
+        <div class="card color__block">
+          <h2>RGB/HEX</h2>
+          <div class="color__range">
+            <label>Red</label>
+            <input type="range" id="rgb-redRange" name="rgb-redRange" min="0" max="255" step="1" value="255">
+            <input type="number" id="rgb-redNumber" name="rgb-redRange" min="0" max="255" step="1" value="255">
+          </div>
+          <div class="color__range">
+            <label>Green</label>
+            <input type="range" id="rgb-greenRange" name="rgb-greenRange" min="0" max="255" step="1" value="255">
+            <input type="number" id="rgb-greenNumber" name="rgb-greenRange" min="0" max="255" step="1" value="255">
+          </div>
+          <div class="color__range">
+            <label>Blue</label>
+            <input type="range" id="rgb-blueRange" name="rgb-blueRange" min="0" max="255" step="1" value="255">
+            <input type="number" id="rgb-blueNumber" name="rgb-blueRange" min="0" max="255" step="1" value="255">
+          </div>
+        </div>
+        <div class="card color__block">
+          <h2>RGB/HEX</h2>
+        </div>
+        <div class="card color__block">
+          <h2>RGB/HEX</h2>
+        </div>
+      </main>
+
     `;
   }
+
+  _addSwatch() {
+    const newSwatch = { x: 0.9505,
+      y: 1.0000,
+      z: 1.0888,
+      hex: '#ffffff',
+      contrast: '1.0:1'}
+    this.swatches.push(newSwatch);
+    this.requestUpdate();
+  }
+
+  _remSwatch(i) {
+    this.swatches = this.swatch.splice(i, 1);
+  }
+
+  _setSwatch(i){
+    this.selectedSwatch = i;
+    const xyz = [this.swatches[i].x, this.swatches[i].y, this.swatches[i].z]
+    this._setColor([...xyz]);
+    console.log(i);
+  }
+
+  _setColor(x, y, z) {
+    const xyz = (x, y, z);
+    const rgb = this._rgbFromXyz(x, y, z);
+    const hex = this._hexFromRgb([...rgb]);
+    this.color = {
+      xyz,
+      rgb,
+      hex,
+      value: hex
+    }
+  }
+
+  _rgbFromXyz(x, y, z) { // https://stackoverflow.com/questions/43494018/converting-xyz-color-to-rgb
+    const r = 3.2404542*x - 1.5371385*y - 0.4985314*z;
+    const g = -0.9692660*x + 1.8760108*y + 0.0415560*z;
+    const b = 0.0556434*x - 0.2040259*y + 1.0572252*z;
+    return([r, g, b]);
+  }
+
+  _hexFromRgb(r, g, b) { // https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+    const rgb = b | (g << 8) | (r << 16);
+    const hex = '#' + (0x1000000 + rgb).toString(16).slice(1);     
+    return hex;
+  }
+
+  _swatchTemplate(swatch, i) {
+    return html`<div class="card swatch ${(this.selectedSwatch === i) ? 'swatch--selected' : ''}" style="background-color: ${swatch.hex}">
+      <input type="radio" value="${swatch.hex}" class="swatch__radio" @click=${this._setSwatch(i)}>
+      <label class="swatch__label">
+        <h3 class="swatch__title">${swatch.hex}</h3>
+        <h4 class="swatch__contrast">${swatch.contrast}</h4>    
+      </label>
+    </div>`;
+  }
 }
+
+
 
 customElements.define('color-tool', ColorTool);
